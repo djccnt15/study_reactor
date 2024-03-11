aeebd8b107f74d48d145b4c70900a84a72185141
Jay.D <djccnt15@gmail.com>
2024-03-12 00:02:34 +0900

    create basic reactor Publisher

--- src/main/java/org/study_reactor/Main.java
+++ src/main/java/org/study_reactor/Main.java
@@ -2,6 +2,18 @@ package org.study_reactor;
 
 public class Main {
     public static void main(String[] args) {
+        
+        var publisher = new Publisher();
+        
+        publisher.startFlux()
+                .subscribe(System.out::print);
+        
+        publisher.startMono()
+                .subscribe();
+        
+        publisher.startMonoEmpty()
+                .subscribe();
+        
         System.out.print("Hello and welcome!");
     }
 }
---
+++ src/main/java/org/study_reactor/Publisher.java
@@ -0,0 +1,24 @@
+package org.study_reactor;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class Publisher {
+    
+    public Flux<Integer> startFlux() {
+        // return Flux.just(1, 2, 3, 4, 5);
+        // return Flux.fromIterable(List.of(1, 2, 3, 4));
+        return Flux.range(1, 5).log();
+    }
+    
+    public Mono<Integer> startMono() {
+        // return Mono.justOrEmpty(1);
+        return Mono.just(1).log();
+    }
+    
+    public Mono<?> startMonoEmpty() {
+        return Mono.empty().log();
+    }
+}
